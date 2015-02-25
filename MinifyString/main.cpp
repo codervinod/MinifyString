1a27014d9049e7fd2677d5d4706905728c9eb6c2
Vinod Gupta <vinod_gupta@live.com>
2015-02-25 15:27:46 -0800

    Initial commit

--- MinifyString/main.cpp
+++ MinifyString/main.cpp
@@ -8,8 +8,57 @@
 
 #include <iostream>
 
+
+void minifyStrUtil(char *str)
+{
+    int i=0,j=0;
+    while(str[i] !=' ' && str[i] != '\0')
+        ++i;
+    
+    if(str[i] == '\0')
+        return;
+    
+    j=i+1;
+    while(str[j] == ' ' && str[j] != '\0')
+        ++j;
+    
+    if(str[j] == '\0')
+    {
+        str[i+1] = '\0';
+        return;
+    }
+    
+    int tmp=i+1;
+    while(str[j] != ' ' && str[j] !='\0')
+    {
+        str[i+1] = str[j];
+        str[j] = ' ';
+        ++i;
+        ++j;
+    }
+    
+    minifyStrUtil(str+tmp);
+}
+
+
+void minifyStr(const char*str)
+{
+    char *mystr = (char*)str;
+    int i=0;
+    //REMOVE PREFIX SPACE IF ANY
+    while(mystr[i] == ' ' && mystr[i+1] != '\0')
+    {
+        mystr[i]=mystr[i+1];
+        mystr[i+1] = ' ';
+        ++i;
+    }
+    minifyStrUtil((char*)mystr);
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    char str[]=" I     love     my     India. ";
+    std::cout << "Original string:"<<str<<"\n";
+    minifyStr(str);
+    std::cout << "Minified string:"<<str<<"\n";
     return 0;
 }
